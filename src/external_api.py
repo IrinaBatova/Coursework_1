ac51edeacda6059def4a136cdc70c0c0d7e86a8d
Irina Batova <BatIr_74_2@mail.ru>
2026-02-17 23:14:47 +0300

    Added functions currency_rate, stock_prices to the external_api.py module

--- src/external_api.py
+++ src/external_api.py
@@ -1,5 +1,6 @@
 import json
 import os
+from pprint import pprint
 
 import requests
 from dotenv import load_dotenv
@@ -14,7 +15,7 @@ def currency_conversion(amount: str, currency: str) -> float:
     """
 
     load_dotenv()
-    api_key = os.getenv("API_KEY")
+    api_key = os.getenv("API_KEY_APILayer")
 
     url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
     payload: dict = {}
@@ -46,10 +47,90 @@ def currency_conversion(amount: str, currency: str) -> float:
     return 0.0
 
 
+def currency_rate (base: str, symbols: str) -> dict:
+    """
+    Функция для получения текущего курса валюты.
+    - param base: принимает тип валюты в виде строки;
+    - param symbols: список разделенных запятыми кодов валют в виде строки
+    - return: возвращает курс, заданных валют в рублях в виде словаря, где
+    ключ это тип валюты, а значение курс валюты в рублях.
+    """
+
+    load_dotenv()
+    api_key = os.getenv("API_KEY_APILayer")
+
+    url = f"https://api.apilayer.com/exchangerates_data/latest?symbols={symbols}&base={base}"
+
+    payload: dict = {}
+    headers = {"apikey": f"{api_key}"}
+
+    try:
+        response = requests.request("GET", url=url, headers=headers, data=payload)
+        result = response.json()  # извлекаем данные из ответа в формате JSON и преобразуем их в Python-словарь (dict)
+
+        if "rates" in result:
+            return result["rates"]
+        else:
+            print(result)
+
+    except requests.exceptions.RequestException as e:
+        print(f"Сообщение об ошибке: {e}")
+
+    except json.JSONDecodeError as e:
+        print("Ошибка декодирования. Invalid result.")
+        print(f"Сообщение об ошибке: {e.msg}")
+        print(f"Строка: {e.lineno}, колонка: {e.colno}")
+
+    except TypeError:
+        print("The object type is not serializable in JSON format.")
+
+    except Exception as e:
+        print(f"Ошибка {e}")
+
+    return {}
+
+def stock_prices (symbols: str) -> dict:
+    """
+    Функция для получения стоимости акции.
+    - param symbols: принимает тикер акции в виде строки;
+    - return: возвращает стоимость акции в виде словаря.
+    """
+
+    load_dotenv()
+    api_key = os.getenv("API_KEY_Alpha_Vantage")
+
+    url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbols}&apikey={api_key}'
+
+    try:
+        r = requests.get(url)
+        data = r.json()
+
+        return data
+
+    except requests.exceptions.RequestException as e:
+        print(f"Сообщение об ошибке: {e}")
+
+    except json.JSONDecodeError as e:
+        print("Ошибка декодирования. Invalid result.")
+        print(f"Сообщение об ошибке: {e.msg}")
+        print(f"Строка: {e.lineno}, колонка: {e.colno}")
+
+    except TypeError:
+        print("The object type is not serializable in JSON format.")
+
+    except Exception as e:
+        print(f"Ошибка {e}")
+
+    return {}
+
 
 if __name__ == "__main__":
 
-    print(currency_conversion("8221.37", "USD"))
+    # print(currency_conversion("8221.37", "USD"))
+    # print(currency_rate("RUB", "USD,EUR,CNY"))
+    # print(currency_rate("USD", "RUB"))
+    pprint(stock_prices("AAPL"))
+
 
 
 # Результат response.json() - {'success': True,
