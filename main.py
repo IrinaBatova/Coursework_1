6032320e1ed8f1bffe590b9c72d0ece7be921fa0
Irina Batova <BatIr_74_2@mail.ru>
2026-02-18 22:19:38 +0300

    Добавлен модуль main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from src import views
+from pprint import pprint
+
+
+
+
+
+if __name__ == "__main__":
+    data_user = "2020-06-28 13:30:35"
+    pprint(views.main_page(data_user))
+    pprint(views.main_page(
+
+    ))
