fcbfb3194d1772813cf081a972c4218ad1e15bb4
Irina Batova <BatIr_74_2@mail.ru>
2026-02-22 20:18:16 +0300

    Added logging to the views.py module of the src package.

--- src/views.py
+++ src/views.py
@@ -1,9 +1,20 @@
+import logging
+import inspect
 from pprint import pprint
 from datetime import datetime
 from typing import Any, Dict
 from pathlib import Path
 from src.data_import import read_excel_file
 from src.utils import get_greeting, get_time_period, get_cards, get_top_transactions, get_currency_rates, get_stock_prices
+
+log_path = Path(__file__).parent.parent / "logs" / "views.log"
+
+logger = logging.getLogger("views")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(log_path, encoding="utf-8", mode="w")
+file_formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 file_path = str(Path(__file__).parent.parent / "data")
@@ -17,34 +28,48 @@ def main_page(data_user: str=datetime.now().strftime("%Y-%m-%d %H:%M:%S")) -> Di
     :return: JSON-ответ
     """
 
-    # Приветствие
-    greeting = get_greeting(data_user)
+    # Получаем имя текущей функции
+    func_name = inspect.currentframe().f_code.co_name
 
-    # Период с начала месяца по заданную пользователем дату
-    period = get_time_period(data_user)
+    logger.info(f'Начала выполняться функция "{func_name}"')
 
-    # Список транзакций за заданный период формата ["01.05.2020", "20.05.2020"]
-    list_of_transactions = read_excel_file(path_to_file=f"{file_path}/operations.xlsx", time_period=period)
+    try:
 
-    # По каждой карте (последние 4 цифры карты, общая сумма расходов, кешбэк (1 рубль на каждые 100 рублей):
-    cards = get_cards(list_of_transactions)
+        # Приветствие
+        greeting = get_greeting(data_user)
 
-    # Топ-5 транзакций по сумме платежа
-    top_transactions = get_top_transactions(list_of_transactions)
+        # Период с начала месяца по заданную пользователем дату
+        period = get_time_period(data_user)
 
-    # Курс валют
-    currency_rates = get_currency_rates("RUB")
+        # Список транзакций за заданный период формата ["01.05.2020", "20.05.2020"]
+        list_of_transactions = read_excel_file(path_to_file=f"{file_path}/operations.xlsx", time_period=period)
 
-    # Стоимость акций из S&P500
-    stock_prices = get_stock_prices()
+        # По каждой карте (последние 4 цифры карты, общая сумма расходов, кешбэк (1 рубль на каждые 100 рублей):
+        cards = get_cards(list_of_transactions)
 
-    return {
-        "greeting": greeting,
-        "cards": cards,
-        "top_transactions": top_transactions,
-        "currency_rates": currency_rates,
-        "stock_prices": stock_prices
-    }
+        # Топ-5 транзакций по сумме платежа
+        top_transactions = get_top_transactions(list_of_transactions)
+
+        # Курс валют
+        currency_rates = get_currency_rates("RUB")
+
+        # Стоимость акций из S&P500
+        stock_prices = get_stock_prices()
+
+        logger.info(f'Функция "{func_name}" возвратила JSON-ответ')
+
+        return {
+            "greeting": greeting,
+            "cards": cards,
+            "top_transactions": top_transactions,
+            "currency_rates": currency_rates,
+            "stock_prices": stock_prices
+        }
+
+    except Exception as ex:
+        logger.info(f'Функция "{func_name}" возвратила ошибку общее исключение: {ex}')
+        print(f'Функция "{func_name}" возвратила ошибку общее исключение:{ex}')
+
 
 if __name__ == "__main__":
     pprint(main_page("2021-12-31 13:57:29"))
