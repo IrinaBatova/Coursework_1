392573eeaee17e1d2bb567dd2d36ed22c60176ae
Irina Batova <BatIr_74_2@mail.ru>
2026-03-12 17:42:51 +0300

    Changes have been added to the reports.py and test_reports.py modules.

--- src/reports.py
+++ src/reports.py
@@ -2,7 +2,6 @@ import logging
 import inspect
 import os
 from pprint import pprint
-
 import pandas as pd
 from datetime import datetime
 from pathlib import Path
@@ -37,48 +36,54 @@ def spending_by_category(df_transactions: pd.DataFrame, category: str, date: Opt
 
     logger.info(f'Начала выполняться функция "{func_name}"')
 
-    if date is None:
-        date = datetime.now().strftime("%d.%m.%Y")
+    try:
 
-    # Преобразуем текущую дату из строки в объект datetime
-    current_date = datetime.strptime(date, "%d.%m.%Y").date()
+        if date is None:
+            date = datetime.now().strftime("%d.%m.%Y")
 
-    # Вычитаем из текущей даты три месяца
-    three_months_ago = current_date - relativedelta(months=3)
+        # Преобразуем текущую дату из строки в объект datetime
+        current_date = datetime.strptime(date, "%d.%m.%Y").date()
 
-    # Преобразуем столбец 'Дата операции' из типа str в тип datetime64
-    df_transactions['Дата операции'] = pd.to_datetime(df_transactions['Дата операции'], format='%d.%m.%Y %H:%M:%S')
-    # print(df_transactions['Дата операции'][0])
-    # print(df_transactions['Дата операции'].dtype)
+        # Вычитаем из текущей даты три месяца
+        three_months_ago = current_date - relativedelta(months=3)
 
-    # Локализация (метод .dt.tz_localize('UTC') добавляет временную зону UTC) и
-    # конвертация (метод .dt.tz_convert('Europe/Moscow') преобразует времена из UTC в московское время) временной зоны
-    s_local = df_transactions['Дата операции'].dt.tz_localize('UTC').dt.tz_convert('Europe/Moscow')
-    # pprint(s_local[0])
-    # print(f'Тип переменной s_local: {type(s_local[0])}')
+        # Преобразуем столбец 'Дата операции' из типа str в тип datetime64
+        df_transactions['Дата операции'] = pd.to_datetime(df_transactions['Дата операции'], format='%d.%m.%Y %H:%M:%S')
+        # print(df_transactions['Дата операции'][0])
+        # print(df_transactions['Дата операции'].dtype)
 
-    # Создаём маску (или фильтр) для данных. Используется метод between(), чтобы выбрать даты, которые находятся в диапазоне
-    # между three_months_ago и current_date, включительно (inclusive="both"). Возвращает логическую маску (массив True/False)
-    mask = s_local.dt.date.between(three_months_ago, current_date, inclusive="both")
+        # Локализация (метод .dt.tz_localize('UTC') добавляет временную зону UTC) и
+        # конвертация (метод .dt.tz_convert('Europe/Moscow') преобразует времена из UTC в московское время) временной зоны
+        s_local = df_transactions['Дата операции'].dt.tz_localize('UTC').dt.tz_convert('Europe/Moscow')
+        # pprint(s_local[0])
+        # print(f'Тип переменной s_local: {type(s_local[0])}')
 
-    # Создаём новый DataFrame df_transactions_filtered, состоящий только из строк df_transactions,
-    # даты которых находятся в указанном диапазоне
-    df_transactions_filtered = df_transactions[mask]
-    # pprint(df_transactions_filtered)
-    # print(df_transactions_filtered['Дата операции'].dtype)
+        # Создаём маску (или фильтр) для данных. Используется метод between(), чтобы выбрать даты, которые находятся в диапазоне
+        # между three_months_ago и current_date, включительно (inclusive="both"). Возвращает логическую маску (массив True/False)
+        mask = s_local.dt.date.between(three_months_ago, current_date, inclusive="both")
 
-    # Проверяем пустой ли DataFrame
-    if not df_transactions_filtered.empty:
+        # Создаём новый DataFrame df_transactions_filtered, состоящий только из строк df_transactions,
+        # даты которых находятся в указанном диапазоне
+        df_transactions_filtered = df_transactions[mask]
+        # pprint(df_transactions_filtered)
+        # print(df_transactions_filtered['Дата операции'].dtype)
 
-        # # Создаём новый DataFrame, состоящий из строк, где значение 'Сумма операции' < 0, а
-        # # значение 'Категория' совпадает со значением заданной категории 'category'
-        df_result = df_transactions_filtered[(df_transactions_filtered['Сумма операции'] < 0) & (df_transactions_filtered['Категория'] == category)]
-        logger.info(f'Функция "{func_name}" возвратила DataFrame')
-        return df_result
+        # Проверяем пустой ли DataFrame
+        if not df_transactions_filtered.empty:
 
-    else:
-        logger.info(f'Функция "{func_name}" возвратила пустой DataFrame, нет транзакций за заданный период')
-        print(f'Функция "{func_name}" возвратила пустой DataFrame, нет транзакций за заданный период')
+            # # Создаём новый DataFrame, состоящий из строк, где значение 'Сумма операции' < 0, а
+            # # значение 'Категория' совпадает со значением заданной категории 'category'
+            df_result = df_transactions_filtered[(df_transactions_filtered['Сумма операции'] < 0) & (df_transactions_filtered['Категория'] == category)]
+            logger.info(f'Функция "{func_name}" возвратила DataFrame')
+            return df_result
+
+        else:
+            logger.info(f'Функция "{func_name}" возвратила пустой DataFrame, нет транзакций за заданный период')
+            print(f'Функция "{func_name}" возвратила пустой DataFrame, нет транзакций за заданный период')
+
+    except Exception as ex:
+        logger.info(f'Функция "{func_name}" возвратила ошибку общее исключение: {ex}')
+        raise Exception(f'Функция "{func_name}" возвратила ошибку общее исключение:{ex}')
 
 
 
@@ -133,7 +138,7 @@ if __name__ == "__main__":
 
     # Вызов БЕЗ декоратора
     # pprint(spending_by_category(df_data, 'Супермаркеты'))
-    # pprint(spending_by_category(df_data, 'Супермаркеты', "05.12.2021"))
+    pprint(spending_by_category(df_data, 'Супермаркеты', "05.12.2021"))
     # pprint(spending_by_category(df_data, 'Супермаркеты', "05.12.2026"))
 
 
